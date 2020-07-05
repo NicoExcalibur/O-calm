9ebf369615793ce9f87706250448ad7484e6e440
Fidia-e <elbouanani.f@gmail.com>
2020-07-05 18:12:35 +0200

    custom post type and his taxonomies for adding content

---
+++ content/plugins/ocalm-settings/inc/video_cpt.php
@@ -0,0 +1,149 @@
+<?php
+
+if (!defined('WPINC')) {
+  die;
+}
+
+class Video_cpt 
+{
+
+    public function __construct()
+    {
+      add_action('init', [$this, 'create_cpt']);
+      add_action('init', [$this, 'create_taxo']);
+    }
+  
+
+    public function create_cpt()
+    {
+      $labels = [
+        'name'               => 'Vidéos',
+        'singular_name'      => 'vidéo',
+        'menu_name'          => 'oCalm',
+        'name_admin_bar'     => 'vidéo',
+        'add_new'            => 'Ajouter une vidéo',
+        'add_new_item'       => 'Ajouter une nouvelle vidéo',
+        'new_item'           => 'Nouvelle vidéo',
+        'edit_item'          => 'Editer une vidéo',
+        'view_item'          => 'Voir la vidéo',
+        'all_items'          => 'Voir toutes les vidéos',
+        'search_items'       => 'Rechercher une vidéo',
+        'not_found'          => 'Aucune vidéo trouvée',
+        'not_found_in_trash' => 'Aucune vidéo trouvée dans la corbeille',
+      ];
+  
+      $args = [
+        'label' => 'Vidéos',
+        'labels' => $labels,
+        'public' => true,
+        'hierarchical' => false,
+        'show_in_rest' => false,
+        'menu_position' => 4,
+        'menu_icon' => 'dashicons-video-alt3',
+        'supports' => [
+          'title',
+          'catégorie',
+          'editor',
+          'excerpt',
+          'thumbnail',
+          'custom-fields'
+        ],
+        'has_archive' => true,
+        'rewrite' => [
+          'slug' => 'videos',
+          'with_front' => true
+        ]
+  
+      ];
+      // https://developer.wordpress.org/reference/functions/register_post_type/
+      register_post_type('video', $args);
+    }
+
+
+    public function create_taxo()
+    {
+        $labels = [
+            'name'                       => 'Catégories',
+            'singular_name'              => 'Catégorie',
+            'menu_name'                  => 'Catégories',
+            'all_items'                  => 'Toutes les catégories',
+            'new_item_name'              => 'Nouvelle catégorie',
+            'add_new_item'               => 'Ajouter une catégorie',
+            'update_item'                => 'Mettre à jour une catégorie',
+            'edit_item'                  => 'Editer une catégorie',
+            'view_item'                  => 'Voir toutes les catégories',
+            'separate_items_with_commas' => 'Séparer les catégories avec une virgule',
+            'add_or_remove_items'        => 'Ajouter ou supprimer une catégorie',
+            'choose_from_most_used'      => 'Choisir parmis les catégories les plus utilisées',
+            'popular_items'              => 'Catégories populaires',
+            'search_items'               => 'Rechercher une catégorie',
+            'not_found'                  => 'Aucune catégorie trouvée',
+            'items_list'                 => 'Lister les catégories',
+        ];
+
+        $args = [
+            'labels'        => $labels,
+            'hierarchical'  => true,
+            'public'        => true,
+          //   'capabilities'      => [
+          //     'manage_terms'  => 'edit_videos',
+          //     'edit_terms'    => 'edit_videos',
+          //     'delete_terms'  => 'delete_videos',
+          //     'assign_terms'  => 'edit_videos',
+          // ]
+        ];
+
+        register_taxonomy('categorie', 'video', $args);
+
+        $labels = [
+            'name'                       => 'Auteurs',
+            'singular_name'              => 'Auteur',
+            'menu_name'                  => 'Auteurs',
+            'all_items'                  => 'Tous les auteurs',
+            'new_item_name'              => 'Nouvel auteur',
+            'add_new_item'               => 'Ajouter un auteur',
+            'update_item'                => 'Mettre à jour un auteur',
+            'edit_item'                  => 'Editer un auteur',
+            'view_item'                  => 'Voir tous les auteurs',
+            'separate_items_with_commas' => 'Séparer les auteurs avec une virgule',
+            'add_or_remove_items'        => 'Ajouter ou supprimer un auteur',
+            'choose_from_most_used'      => 'Choisir parmis les auteurs les plus utilisés',
+            'popular_items'              => 'Auteurs populaires',
+            'search_items'               => 'Rechercher un auteur',
+            'not_found'                  => 'Aucun auteur trouvé',
+            'items_list'                 => 'Lister les auteurs',
+            'items_list_navigation'      => 'Lister les auteurs',
+        ];
+
+        $args = [
+            'labels'            => $labels,
+            'hierarchical'      => false,
+            'public'            => true,
+            // 'capabilities'      => [
+            //     'manage_terms'  => 'edit_videos',
+            //     'edit_terms'    => 'edit_videos',
+            //     'delete_terms'  => 'delete_videos',
+            //     'assign_terms'  => 'edit_videos',
+            // ]
+        ];
+
+        register_taxonomy('auteur', 'video', $args);
+    }
+
+    public function activation()
+    {
+        $this->create_cpt();
+        $this->create_taxo();
+
+        flush_rewrite_rules();
+    }
+
+    public function deactivation()
+    {
+        flush_rewrite_rules();
+    }
+ 
+}
+
+
+
--- content/plugins/ocalm-settings/ocalm-settings.php
+++ content/plugins/ocalm-settings/ocalm-settings.php
@@ -1,7 +1,7 @@
 <?php
 /*
-Plugin Name: Ocalm Settings
-Description: Réglages de Ocalm
+Plugin Name: oCalm Settings
+Description: Réglages de oCalm
 Author: Fidia, Nico, Vincent
 Version: 1.0.0
 */
@@ -10,7 +10,13 @@ Version: 1.0.0
 if (!defined('WPINC')) {die();}
 
 // Inclusion des différentes classes necessaire au plugin
-require plugin_dir_path(__FILE__) . 'inc/cpt.php';
-require plugin_dir_path(__FILE__) . 'inc/api_rest.php';
+require plugin_dir_path(__FILE__) . 'inc/video_cpt.php';
+require plugin_dir_path(__FILE__) . 'inc/rest_api.php';
 
 //
+
+// CPT RECIPE + TAXO
+$video_cpt = new Video_cpt();
+
+register_activation_hook(__FILE__, [$video_cpt, 'activation']);
+register_deactivation_hook(__FILE__, [$video_cpt, 'deactivation']);
