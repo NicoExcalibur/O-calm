f79e1d3781962c1b4fc33b4abd9271800480fbe0
NicoExcalibur <nico.zanz@gmail.com>
2020-07-06 15:13:35 +0200

    :construction: adding a function to create a custom table at plugin activation

---
+++ content/plugins/ocalm-settings/inc/custom_table.php
@@ -0,0 +1,17 @@
+<?php
+
+// function that create a custom table in the DB 
+function ocalm_custom_table_install () {
+    global $wpdb;
+
+    $favourite = $wpdb->wp_;
+
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE $favourite (
+    id int NOT NULL AUTO_INCREMENT
+    ) $charset_collate;";
+
+    dbDelta( $sql );    
+}
+
