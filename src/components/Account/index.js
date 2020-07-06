98fe03093ecfad8d64d8d2baf2f5a41c863fc1cc
Geashbey <etudiant@teleporteur.oclock>
2020-07-06 17:09:04 +0200

    account page in progress

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Edit } from 'react-feather';
+
+import avatar from 'src/assets/avatar.png';
+import './account.scss';
 
 const Account = () => (
   <div className="account">
-    Coucou c'est moi l'account!
+    <h1 className="account-title">Coucou MicheMiche</h1>
+    <div className="avatar-editor">
+      <Edit className="edit" />
+      <img className="avatar" src={avatar} alt="avatar" />
+    </div>
+    <form className="edit-info">
+      <input type="mail" className="mail" placeholder="maildugars" />
+      <input type="text" className="pseudo" placeholder="pseudodugars" />
+      <input type="password" className="password" placeholder="desétoilescarcestunpsswrd" />
+      <button type="submit" className="submit">Modifier mes infos</button>
+    </form>
   </div>
 );
 
