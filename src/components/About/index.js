5d1ebbedccc2ade8be5d8ab3ec3cd849ef77450f
manonxrray <manon.gerray@gmail.com>
2020-07-07 17:19:23 +0200

    About page done

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,8 +1,64 @@
 import React from 'react';
 
+import './about.scss';
+
 const About = () => (
   <div className="about">
-    Oui heu bonjour, nous voilà on est ici pour se présenter!
+    <div className="project">
+      <h1 className="about-title">Pourquoi O'Calm ?</h1>
+      <p>
+        Les crottes de fromage, bien sqouick sqouick dans yeule, c'est ça le bonheur. Arrête de faire la
+        baboune tabaslack, a va fondre la slutch.
+        Il m'a même pas redonné mon ptit change, ya essayé de me crosser le câlice. Sa blonde m'en a sacré
+        toute une au parcheesi, mais elle a été mardeuse.
+        Baptême de crisse de cibole de viande à chien, j'en ai plein mon casse d'être toute décalissé de la
+        vie, avec le windshier de mon char toute déviargé par l'autre esti de cave. Des patates pilées, du
+        steak haché, du maïs en crème, pis kin toé, du pâté chinois.
+      </p>
+    </div>
+    <h2 className="about-title">La team du zen</h2>
+    <div className="us">
+      <div className="id-card">
+        <div className="avatar" />
+        <div className="infos">
+          <h2 className="name">Manon Gerray</h2>
+          <h3 className="role">Scrum master</h3>
+          <h4 className="spe">React</h4>
+        </div>
+      </div>
+      <div className="id-card">
+        <div className="avatar" />
+        <div className="infos">
+          <h2 className="name">Axel Cannit</h2>
+          <h3 className="role">Lead dev Front</h3>
+          <h4 className="spe">React</h4>
+        </div>
+      </div>
+      <div className="id-card">
+        <div className="avatar" />
+        <div className="infos">
+          <h2 className="name">Fidia El Bouanani</h2>
+          <h3 className="role">Product Owner</h3>
+          <h4 className="spe">Wordpress</h4>
+        </div>
+      </div>
+      <div className="id-card">
+        <div className="avatar" />
+        <div className="infos">
+          <h2 className="name">Vincent Zepharren</h2>
+          <h3 className="role">Lead dev Back</h3>
+          <h4 className="spe">Wordpress</h4>
+        </div>
+      </div>
+      <div className="id-card">
+        <div className="avatar" />
+        <div className="infos">
+          <h2 className="name">Nicolas Delisle</h2>
+          <h3 className="role">Git master</h3>
+          <h4 className="spe">Wordpress</h4>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Edit } from 'react-feather';
 
-import avatar from 'src/assets/avatar.png';
+import avatar from 'src/assets/fabio.jpg';
 import './account.scss';
 
 const Account = () => (
   <div className="account">
     <div className="desktop">
-      <h2 className="account-name">Bonjour Michemiche !</h2>
+      <h2 className="account-name">Bonjour Fabio !</h2>
       <div className="avatar-editor">
         <Edit className="edit" />
         <img className="avatar" src={avatar} alt="avatar" />
