53418a454ed0d7ba7bbb18255aa574582b95aabe
Geashbey <etudiant@teleporteur.oclock>
2020-07-08 16:28:00 +0200

    request axios ok :ok_hand: :tada:

--- src/middlewares/videosMiddleware.js
+++ src/middlewares/videosMiddleware.js
@@ -5,9 +5,9 @@ import { FETCH_VIDEOS } from 'src/actions/videos';
 const videosMiddleware = (store) => (next) => (action) => {
   switch (action.type) {
     case FETCH_VIDEOS: {
-      axios.get('http://http://ec2-100-25-192-123.compute-1.amazonaws.com/o-calm/wp-json/wp/v2/video')
+      axios.get('http://ec2-100-25-192-123.compute-1.amazonaws.com/o-calm/wp-json/wp/v2/video')
         .then((response) => {
-          console.log(response);
+          console.log(response.data);
         })
         .then((error) => {
           console.warn(error);
