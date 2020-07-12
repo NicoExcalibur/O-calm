00f6f4d8c4aae4314721347e2ef66fa390bc040c
manonxrray <manon.gerray@gmail.com>
2020-07-12 15:02:34 +0200

    Readjustments and single page structure :art:

--- src/components/SinglePage/SingleMedia.js
+++ src/components/SinglePage/SingleMedia.js
@@ -4,6 +4,7 @@ import { useMediaQuery } from '@material-ui/core';
 
 const SingleMedia = () => {
   const matchesMobile = useMediaQuery('(max-width:600px)');
+  const matchesMiddle = useMediaQuery('(max-width:800px');
 
   if (matchesMobile === true) {
     return (
@@ -11,8 +12,19 @@ const SingleMedia = () => {
         <ReactPlayer
           className="player"
           url="https://www.youtube.com/watch?v=HlBYdiXdUa8"
-          width={500}
-          height={300}
+          width={300}
+          height={200}
+        />
+      </div>
+    );
+  } if (matchesMiddle === true) {
+    return (
+      <div className="single-media">
+        <ReactPlayer
+          className="player"
+          url="https://www.youtube.com/watch?v=HlBYdiXdUa8"
+          width={600}
+          height={400}
         />
       </div>
     );
@@ -23,10 +35,11 @@ const SingleMedia = () => {
         className="player"
         url="https://www.youtube.com/watch?v=HlBYdiXdUa8"
         width={800}
-        height={600}
+        height={550}
       />
     </div>
   );
 };
 
 export default SingleMedia;
+
--- src/components/SinglePage/index.js
+++ src/components/SinglePage/index.js
@@ -5,8 +5,39 @@ import './singlePage.scss';
 
 const SinglePage = () => (
   <div className="single-page">
-    Coucou lol ici y aura le pléyeur
     <SingleMedia />
+    <div className="media-infos">
+      <h1 className="title">
+        Titre de la vidéo
+      </h1>
+      <div className="categories">
+        <h3 className="category">
+          #méditation
+        </h3>
+        <h3 className="category">
+          #méditation-guidée
+        </h3>
+      </div>
+      <h2 className="author">
+        Jean-Jacques Goldman
+      </h2>
+      <p className="description">
+        T'es din patate mon gars, complètement dans le champ. Toryeu que c'est cheap cette esti de
+        cochonnerie-là. Les crottes de fromage, bien sqouick sqouick dans yeule, c'est ça le
+        bonheur.
+        Heille les ti-culs, vous er'virez de bord drette là si vous pensez que vous vous écartez
+        dans
+        le bois talheure. Il m'a même pas redonné mon ptit change, ya essayé de me crosser le
+        câlice.
+        Je crisse mon camp d'icite anyway, je déguedine au plus sacrant, ça sent la cibole de marde.
+
+        Yé un peu niaiseux le niochon de voisin, il crisse eu'rien avec sa vie. Quessé tu veux j'te
+        dise sacrament, fouille-moé bout d'viarge, je le sais-tu moé. Osti qui fa frette. J'ai
+        finalement pogné la twist pour faire la poutine. Je lui ai garôché une estie de garnotte en
+        plein dans sa crisse de face de crosseur, il en menait pas large je te jure. Lâche pas la
+        patate, enwoye, tu rentres la gogosse dans la slot pis ça va le faire.
+      </p>
+    </div>
   </div>
 );
 
