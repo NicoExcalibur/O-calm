0799745580b81f472bfcfd6d19ef2f61647feb59
manonxrray <manon.gerray@gmail.com>
2020-07-12 14:10:39 +0200

    Width and height of video changing while on mobile or desktop thx to material-ui :tada:

--- src/components/SinglePage/SingleMedia.js
+++ src/components/SinglePage/SingleMedia.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import ReactPlayer from 'react-player';
+import { useMediaQuery } from '@material-ui/core';
 
-const SingleMedia = () => (
-  <div className="single-media">
-    Coucou lol c moi le media qui sera en gros lol lol lol
-  </div>
-);
+const SingleMedia = () => {
+  const matchesMobile = useMediaQuery('(max-width:600px)');
+
+  if (matchesMobile === true) {
+    return (
+      <div className="single-media">
+        <ReactPlayer
+          className="player"
+          url="https://www.youtube.com/watch?v=HlBYdiXdUa8"
+          width={500}
+          height={300}
+        />
+      </div>
+    );
+  }
+  return (
+    <div className="single-media">
+      <ReactPlayer
+        className="player"
+        url="https://www.youtube.com/watch?v=HlBYdiXdUa8"
+        width={800}
+        height={600}
+      />
+    </div>
+  );
+};
 
 export default SingleMedia;
