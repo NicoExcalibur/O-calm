b6db8e53b414196ecda337123206f38454ece6a1
Axel Cannit <etudiant@teleporteur.oclock>
2020-07-13 22:57:14 +0200

    try for picture 6

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
+import vincent from 'src/assets/images/vct.png';
+import manon from 'src/assets/images/mnn.png';
+import axel from 'src/assets/images/axl.png';
+import nico from 'src/assets/images/ncl.png';
+import fidia from 'src/assets/images/fid.png';
+import headphone from 'src/assets/images/headphone.png';
+import avatar from 'src/assets/images/fabio.png';
+
+
 import Home from 'src/containers/Home';
 import Research from 'src/containers/Research';
 import Favorites from 'src/containers/Favorites';
+import SinglePage from 'src/containers/SinglePage';
 import Account from '../Account';
 import LegalMentions from '../LegalMentions';
 import About from '../About';
-import SinglePage from 'src/containers/SinglePage';
 
 const Page = () => (
   <div className="page">
