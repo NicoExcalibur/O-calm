4797156bb32b2942185ccc81b8eb073267833926
Geashbey <axelcannit@orange.fr>
2020-07-14 22:33:54 +0200

    auth in :construction:

--- src/middlewares/usersMiddleware.js
+++ src/middlewares/usersMiddleware.js
@@ -20,7 +20,11 @@ const usersMiddleware = (store) => (next) => (action) => {
 
     case VERIF_LOGIN: {
       const { loginValue } = store.getState().users;
-      axios.post('http://ec2-100-25-192-123.compute-1.amazonaws.com/o-calm/wp-json/jwt-auth/v1/', loginValue)
+      axios.post('http://ec2-100-25-192-123.compute-1.amazonaws.com/o-calm/wp-json/jwt-auth/v1/token', {
+        loginValue,
+      }, {
+        withCredentials: true,
+      })
         .then((response) => {
           console.log(response);
           // store.dispatch(saveToken(response.data));
