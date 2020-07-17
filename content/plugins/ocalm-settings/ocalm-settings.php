e933a386ac2cb79b6ded8bdaad789b48dcd21ecf
NicoExcalibur <nico.zanz@gmail.com>
2020-07-17 16:40:56 +0200

    :wastebasket: cleaning new function + remove useless file

--- content/plugins/ocalm-settings/ocalm-settings.php
+++ content/plugins/ocalm-settings/ocalm-settings.php
@@ -11,7 +11,6 @@ if (!defined('WPINC')) {die();}
 
 // Inclusion des différentes classes necessaire au plugin
 require plugin_dir_path(__FILE__) . 'inc/video_cpt.php';
-//require plugin_dir_path(__FILE__) . 'inc/custom_table.php';
 require plugin_dir_path(__FILE__) . 'inc/favorite_endpoint.php';
 require plugin_dir_path(__FILE__) . 'inc/roles.php';
 require plugin_dir_path(__FILE__) . 'inc/rest_api.php';
@@ -24,13 +23,6 @@ $video_cpt = new Video_cpt();
 register_activation_hook(__FILE__, [$video_cpt, 'activation']);
 register_deactivation_hook(__FILE__, [$video_cpt, 'deactivation']);
 
-// call the function at plugin activation
-/* $custom_table = new Custom_table();
-
-register_activation_hook( __FILE__,[$custom_table, 'activation']);
-register_deactivation_hook( __FILE__,[$custom_table, 'deactivation']); 
- */
-
 // Api rest
 
 $calm_rest_api = new OcalmRestApi();
@@ -41,8 +33,8 @@ $ocalm_role = new Ocalm_role();
 register_activation_hook(__FILE__, [$ocalm_role, 'activation']);
 register_deactivation_hook(__FILE__, [$ocalm_role, 'deactivation']);
 
-// Favorites Endpoint
-$favorite_endpoint = new FavoriteEndpoint();
+// Create a favorite table + Add a favorite post in DB 
+$add_favorite = new Add_Favorite();
 
-//register_activation_hook( __FILE__,[$favorite_endpoint, 'activation']);
-//register_deactivation_hook( __FILE__,[$favorite_endpoint, 'deactivation']); 
+register_activation_hook( __FILE__,[$add_favorite, 'activation']);
+register_deactivation_hook( __FILE__,[$add_favorite, 'deactivation']); 
