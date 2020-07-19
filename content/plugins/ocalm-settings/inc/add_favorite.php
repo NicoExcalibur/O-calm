d3f544521d0b8e8188835dcfbbf88f7f038ab64d
zephdev-92 <vzepharren@gmail.com>
2020-07-19 16:32:25 +0200

    :construction: improvement of our function, retrieval of user id

--- content/plugins/ocalm-settings/inc/add_favorite.php
+++ content/plugins/ocalm-settings/inc/add_favorite.php
@@ -10,23 +10,28 @@ class Add_favorite {
   public function favorite_endpoint($request) {
       register_rest_route('ocalm-settings/v1', 'video/favorite', [
         'methods' => 'POST',
-        'callback' => [$this, 'get_favorite']
+        'callback' => [$this, 'add_favorite']
       ]);
+      register_rest_route('ocalm-settings/v1', 'video/favorite/', [
+        'methods' => 'GET',
+        'callback' => [$this, 'get_favorite']
+      ]); // on verra demain, plus d'inspiration lol
   }
 
-  public function get_favorite($request = null) {
+  public function add_favorite($request = null) {
+    // faire un if pour dire si on est connecté
+    if (is_user_logged_in()) {
+        $response = [];
+        $parameters = $request->get_json_params();
+        $user_id = get_current_user_id();
+        $post_id = intval($parameters['post_id']);
+        $data = array( $user_id, $post_id);
     
-    $response = [];
-    $parameters = $request->get_json_params();
-    $user_id = intval($parameters['user_id']);
-    $post_id = intval($parameters['post_id']);
-    $data = array( $user_id, $post_id); 
-    
-    $response = new WP_REST_Response( $data );
+        $response = new WP_REST_Response($data);
 
-    $this->insert($user_id, $post_id, $response);
-    return $response;
-    
+        $this->insert($user_id, $post_id, $response);
+        return $response;
+    } // faudrai rajouter renvoyer un message d'erreur mais bon lol 
   }
   // https://developer.wordpress.org/reference/classes/wpdb/
   public function insert($user_id, $post_id, $response) { 
