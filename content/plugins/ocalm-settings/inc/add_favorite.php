601dc9fefc5020fdd23a533fc5262931a7cd5167
zephdev-92 <vzepharren@gmail.com>
2020-07-23 22:39:38 +0200

    :construction: addition of a condition to not display favorites when the user has none

--- content/plugins/ocalm-settings/inc/add_favorite.php
+++ content/plugins/ocalm-settings/inc/add_favorite.php
@@ -25,37 +25,40 @@ class Add_favorite {
   }
   public function get_favorite ()
   {
-      // oui là on dois cherche tout les fav du user
-      if (is_user_logged_in()) {
-          global $wpdb;
+    // oui là on dois cherche tout les fav du user
+    if (is_user_logged_in()) {
+      global $wpdb;
 
-          $this->wpdb = $wpdb;
-          $this->table = $wpdb->prefix . 'favorites';
-          $user_id = get_current_user_id();
+      $this->wpdb = $wpdb;
+      $this->table = $wpdb->prefix . 'favorites';
+      $user_id = get_current_user_id();
 
-          // on récupère un array d'objets
-      $favs = $wpdb->get_results("SELECT * FROM {$this->table} WHERE user_id={$user_id}");  // ici on a une tab avec id, post_id, user_id ? ok
-      //print_r($favs);
-      $myFav = [];
-          // ici on filtre le post_id, donc on récupère le post_id
-          foreach ($favs as $fav) {
-              $myFav[] = $fav->post_id; // la on a le post_id on veut rajouté le id dans oui c fabio on a fait ça hier fid      comment le ID fav on le rajouté là ici, oui style $fav->id faut juste rajouté le id. oui mais comment rajouter plusieurs entré non
-          }
+      // on récupère un array d'objets
+    $favs = $wpdb->get_results("SELECT * FROM {$this->table} WHERE user_id={$user_id}");  // ici on a une tab avec id, post_id, user_id ? ok
+    //print_r($favs); die();
+    if (!empty($favs)) {
+       $myFav = [];
+      // ici on filtre le post_id, donc on récupère le post_id
+      foreach ($favs as $fav) {
+          $myFav[] = $fav->post_id; // la on a le post_id on veut rajouté le id dans oui c fabio on a fait ça hier fid      comment le ID fav on le rajouté là ici, oui style $fav->id faut juste rajouté le id. oui mais comment rajouter plusieurs entré non
+      }
 
-          //print_r($myFav); die();
-          // on affiche les post qu'on veut avec leur id.
-          $args = array(
+      //print_r($myFav); die();
+      // on affiche les post qu'on veut avec leur id.
+      $args = array(
         'posts_per_page' => 100,
         'post_type' => 'video',
         'post__in' => $myFav);
-    
-          $posts = get_posts($args);
-          return $posts; // on a que les post donc pas de id du fav. sinon il faut trouvé le moyen de cherche le user_id et post_id pour remonté au id. genre in_array en cherchant. ou faudrai le rajouté
 
-    //$result = array_merge($favs, $fav);
+      $posts = get_posts($args);
+      return $posts; // on a que les post donc pas de id du fav. sinon il faut trouvé le moyen de cherche le user_id et post_id pour remonté au id. genre in_array en cherchant. ou faudrai le rajouté
 
-    //print_r($result);
-      }
+    } 
+   
+  //$result = array_merge($favs, $fav);
+
+  //print_r($result);
+    }
   }
   
   public function add_favorite($request = null) {
@@ -75,9 +78,7 @@ class Add_favorite {
         
           $response = new WP_REST_Response('pas de post',400); // pour que ça marche pour eux faux pull sur le serveur là on peut testé en local insomnia 
         }
-       
-   
-   
+      
         return $response;
     } 
   }
@@ -142,25 +143,25 @@ class Add_favorite {
   // function that drop the custom_table
   public function custom_table_uninstall()
   { 
-      global $wpdb;
+    global $wpdb;
 
-      $this->wpdb = $wpdb;
-      $this->table = $wpdb->prefix . 'favorites';
-      
-      $sql = "DROP TABLE {$this->table};";
+    $this->wpdb = $wpdb;
+    $this->table = $wpdb->prefix . 'favorites';
+    
+    $sql = "DROP TABLE {$this->table};";
 
-      $this->wpdb->query($sql);
+    $this->wpdb->query($sql);
   }
 
   // callbacks
   public function activation()
   {
-      $this->custom_table();
+    $this->custom_table();
   
   }
 
   public function deactivation()
   {
-      $this->custom_table_uninstall();
+    $this->custom_table_uninstall();
   }
-}
+} // manque un ti truuuuuuuuuuuc!!
